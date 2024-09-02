115729db797eff748e7d3507fc9eb2907c872795
Sandy Andryanto <sandy.andryanto.mki@gmail.com>
2024-09-02 18:15:38 +0700

    up

---
+++ backend/src/data/Source.go
@@ -0,0 +1,28 @@
+package utilities
+
+import (
+	"backend/models"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
+	"os"
+)
+
+func SetupDB() *gorm.DB {
+	godotenv.Load(".env")
+	USER := os.Getenv("DB_USERNAME")
+	PASS := os.Getenv("DB_PASSWORD")
+	HOST := os.Getenv("DB_HOST")
+	PORT := os.Getenv("DB_PORT")
+	DBNAME := os.Getenv("DB_DATABASE")
+	URL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", USER, PASS, HOST, PORT, DBNAME)
+	db, err := gorm.Open(os.Getenv("DB_CONNECTION"), URL)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
+func Config() {
+	db := SetupDB()
+}
---
+++ backend/src/middleware/JWTMiddleware.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	service "backend/services"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthorizeJWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		const _BEARER_SCHEMA = "Bearer "
+		authHeader := c.GetHeader("Authorization")
+		if len(strings.TrimSpace(authHeader)) == 0 {
+			c.AbortWithStatus(http.StatusUnauthorized)
+		} else {
+			tokenString := authHeader[len(BEARER_SCHEMA):]
+			token, err := service.JWTAuthService().ValidateToken(tokenString)
+			if token.Valid {
+				claims := token.Claims.(jwt.MapClaims)
+				c.Set("claims", claims)
+				fmt.Println(claims)
+			} else {
+				fmt.Println(err)
+				c.AbortWithStatus(http.StatusUnauthorized)
+			}
+		}
+
+	}
+}
